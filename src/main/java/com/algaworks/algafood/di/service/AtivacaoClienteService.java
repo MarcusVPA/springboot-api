e7a2fb3b0e96e0a8baae878673ecc3b736d7866d
Marcus Vinicius <marcusmayme18@gmail.com>
2022-12-28 22:13:20 -0300

    Desambiguação de beans com @Qualifier

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,8 +1,7 @@
 package com.algaworks.algafood.di.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -11,7 +10,8 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
-	@Autowired(required=false)
+	@Qualifier("urgente")
+	@Autowired()
 	private Notificador notificador;
 
 	public void ativar(Cliente cliente) {
