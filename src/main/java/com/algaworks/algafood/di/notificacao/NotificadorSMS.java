653a0c8baea2912033957a030f586e2f08c440e2
Marcus Vinicius <marcusmayme18@gmail.com>
2022-12-29 00:31:02 -0300

    Desambiguação de beans com anotação customizada

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Qualifier("urgente")
+@TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
 public class NotificadorSMS implements Notificador {
-
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s por SMS através do telefone %s: %s\n", 
